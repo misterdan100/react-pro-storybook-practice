3e33f92f2b436ac0d40969c217ac33188e2a194e
misterdan100 <danielmca60@gmail.com>
2024-10-24 05:19:57 -0500

    storybook ready to build

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,8 +1,43 @@
 import './MyLabel.css'
 
-export function MyLabel() {
+interface Props {
+  /**
+   * Text to display
+   */
+  label: string,
+
+  /**
+   * Label size
+   */
+  size?: 'normal' | 'h1' | 'h2' | 'h3',
+  /**
+   * Capitalize
+   */
+  allCaps?: boolean,
+  /**
+   * Text color
+   */
+  color?: 'text-primary' | 'text-secondary' | 'text-tertiary',
+  /**
+   * Background color
+   */
+  backColor?: string,
+}
+
+export function MyLabel({ 
+  label, 
+  size = 'normal', 
+  allCaps = false, 
+  color = 'text-primary',
+  backColor = '#FFF'
+}: Props) {
   return (
-    <span className='h1'>My Label</span>
+    <span 
+      className={`${size} ${color} label`}
+      style={{
+        backgroundColor: backColor,
+      }}
+    >{ allCaps ? label.toUpperCase() : label }</span>
   )
 }
 
--- src/stories/MyLabel.stories.ts
+++ src/stories/MyLabel.stories.ts
@@ -4,9 +4,15 @@ import { MyLabel } from "../components/MyLabel";
 
 const meta = {
     title: 'UI/labels/My Label',
+    tags: ['autodocs'],
     component: MyLabel,
     parameters: {
         layout: 'centered'
+    },
+    argTypes: {
+        size: { control: 'inline-radio'},
+        allCaps: {control: 'boolean'},
+        backColor: {control: "color"}
     }
 
 } satisfies Meta<typeof MyLabel>;
@@ -16,5 +22,35 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Basic: Story = {
+    args: {
+        label: 'Basic Label',
+        allCaps: false,
+        size: 'normal',
+        color: 'text-primary',
+        backColor: '#fff'
+    }
+}
 
+export const AllCaps: Story = {
+    args: {
+        label: 'All Caps Label',
+        size: 'normal',
+        allCaps: true,
+    }
+}
+
+export const Secondary: Story = {
+    args: {
+        label: 'Secondary Label',
+        size: 'normal',
+        color: "text-secondary",
+    }
+}
+
+export const CustomColor: Story = {
+    args: {
+        label: 'Custom Color Label',
+        size: 'normal',
+        backColor: '#899098'
+    }
 }
